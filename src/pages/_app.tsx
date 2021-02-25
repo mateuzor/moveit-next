9189b4851e91fbd4d05ce7a3ca203135260e2a06
Mateus Ramos <mateus.ramos@instaletecnologia.com.br>
2021-02-24 22:12:45 -0300

    project with next started

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+import '../styles/global.css'
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
 export default MyApp
+
+//Here is everything that will repeat on all pages
